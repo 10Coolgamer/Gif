56f9303c73355537aacf9a72a0c186594e985941
10Coolgamer <orangetree254@gmail.com>
2026-02-12 22:23:41 -0500

    Delete kame directory

--- kame/create_gif.py
+++
@@ -1,6 +0,0 @@
-import imageio.v3 as iio
-filenames = ['rest.png', 'Kame1.png','Kame2.png','Haaaa.png']
-images = [ ]
-for filename in filenames:
-  images.append(iio.imread(filename))
-  iio.imwrite('Kame_Kame_haaaaa.gif', images, duration = 500, loop = 0)
